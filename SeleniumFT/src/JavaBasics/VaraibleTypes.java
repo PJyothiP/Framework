28425201b018b4924f51f4f49c97310610bfd3a3
PJyothiP <lrjyothiprakash4@gmail.com>
2022-03-21 16:57:01 +0530

    New change

--- SeleniumFT/src/JavaBasics/VaraibleTypes.java
+++ SeleniumFT/src/JavaBasics/VaraibleTypes.java
@@ -9,7 +9,7 @@ public class VaraibleTypes {
 		VaraibleTypes vt2 = new VaraibleTypes();
 		VaraibleTypes vt3 = new VaraibleTypes();
 		pi123 = 7.8;
-		//this is a new change
+		//this is a new change   
 		System.out.println("This is static pi value in static method : "+VaraibleTypes.pi123);
 		System.out.println("This is nonstatic pi value of vt1 object in static method :"+vt1.pi234);
 		vt1.pi234 = 5.4;
